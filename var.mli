983925187b331861944e87ace7cac147c33b7bc5
Benoît Montagu <benoit.montagu@m4x.org>
2010-11-05 10:05:04 +0100

    interface for var.ml

--- var.mli
+++ var.mli
@@ -29,25 +29,4 @@ module type S = sig
   val bto_string: bound -> string
 end
 
-module Make (Default: sig val fbase: string val bbase: string end) : S = struct
-  type free = string * int
-  type bound = int
-  let equal (x,n) (y,m) = MyString.equal x y && MyInt.equal n m
-  let make s = (s,0)
-  let to_string (s,n) = if n = 0 then s else (s ^ (string_of_int n))
-  module H = Hashtbl.Make(MyString)
-  let h = H.create 256
-  let sfresh s =
-    let n = try H.find h s with Not_found -> 0 in
-    H.replace h s (n+1) ;
-    (s, n+1)
-  let ffresh (s,_) = sfresh s
-  let fresh () = sfresh Default.fbase
-  let bfresh _bvar = fresh ()
-  let bzero = 0
-  let bone = 1
-  let bsucc = (+) 1
-  let bequal: int -> int -> bool = (=)
-  let bmax: int -> int -> int = max
-  let bto_string n = Default.bbase ^ (string_of_int n)
-end
+module Make (Default: CONFIG) : S
