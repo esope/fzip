9cd9dce159e161cbc065dd759c44b7454d60096e
Benoît Montagu <benoit.montagu@m4x.org>
2010-11-23 18:10:05 +0100

    quick and incomplete emacs mode for syntax highlighting

---
+++ emacs/fzip-mode.el
@@ -0,0 +1,43 @@
+(require 'generic-x) ;; we need this
+
+(define-generic-mode 
+  'fzip-mode
+  nil
+  '("let"
+    "in"
+    "import"
+    "export"
+    "val"
+    "type" 
+    "as"
+    "S"
+    "Pi"
+    "Π"
+    "sigma"
+    "Σ"
+    "forall"
+    "∀"
+    "exists"
+    "∃"
+    "fun"
+    "λ"
+    "Fun"
+    "Λ")
+  '(("=" . 'font-lock-operator)     ;; '=' is an operator
+    ("::" . 'font-lock-builtin)
+    (":" . 'font-lock-builtin)
+    ("→" . 'font-lock-builtin)
+    ("->" . 'font-lock-builtin)
+    ("⇒" . 'font-lock-builtin)
+    ("=>" . 'font-lock-builtin)
+    ("{" . 'font-lock-delimiter-face)
+    ("}" . 'font-lock-delimiter-face)
+    ("[" . 'font-lock-delimiter-face)
+    ("]" . 'font-lock-delimiter-face)
+    ("<" . 'font-lock-delimiter-face)
+    (">" . 'font-lock-delimiter-face)
+    )
+  '("\\.fzip$")           
+  nil                    
+  "A mode for Fzip files")
+
